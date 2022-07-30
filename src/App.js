2857e8944cce70b61ba73f69108dcfbb6b084407
Peter Liu <peterliucy1108@gmail.com>
2022-07-30 02:30:09 -0700

    Created a new page with name, price and description, connected to backend and store into mongodb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { useState, useEffect, useCallback } from 'react';
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { useState, useEffect, useCallback } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
 
 import Login from "./components/Login";
 import Logout from "./components/Logout";
@@ -14,138 +15,155 @@ import Movie from "./components/Movie";
 import AddReview from "./components/AddReview";
 import FavoritesDataService from "./services/favorites";
 import Favorites from "./components/Favorites";
+import UploadItem from "./components/UploadItem";
 
 import "./App.css";
 
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 function App() {
+	const [user, setUser] = useState(null);
+	const [favorites, setFavorites] = useState([]);
 
-  const [user, setUser] = useState(null);
-  const [favorites, setFavorites] = useState([]);
+	const addFavorite = (movieId) => {
+		setFavorites([...favorites, movieId]);
+	};
 
-  const addFavorite = (movieId) => {
-    setFavorites([...favorites, movieId])
-  }
+	const deleteFavorite = (movieId) => {
+		setFavorites(favorites.filter((f) => f !== movieId));
+	};
 
-  const deleteFavorite = (movieId) => {
-    setFavorites(favorites.filter(f => f !== movieId));
-  }
+	useEffect(() => {
+		let loginData = JSON.parse(localStorage.getItem("login"));
+		if (loginData) {
+			let loginExp = loginData.exp;
+			let now = Date.now() / 1000;
+			if (now < loginExp) {
+				// Not expired
+				setUser(loginData);
+			} else {
+				// Expired
+				localStorage.setItem("login", null);
+			}
+		}
+	}, []);
 
-  useEffect(() => {
-    let loginData = JSON.parse(localStorage.getItem("login"));
-    if (loginData) {
-      let loginExp = loginData.exp;
-      let now = Date.now()/1000;
-      if (now < loginExp) {
-        // Not expired
-        setUser(loginData);
-      } else {
-        // Expired
-        localStorage.setItem("login", null);
-      }
-    }
-  }, []);
+	const retrieveFavorites = useCallback(() => {
+		if (user) {
+			FavoritesDataService.getFavorites(user.googleId)
+				.then((response) => {
+					console.log(response);
+					setFavorites(response.data.favorites);
+				})
+				.catch((e) => {
+					console.log(e);
+				});
+		}
+	}, [user]);
 
+	const updateFavorites = useCallback(() => {
+		if (user) {
+			FavoritesDataService.updateFavorites({
+				_id: user.googleId,
+				favorites: favorites,
+			}).catch((e) => {
+				console.log(e);
+			});
+		}
+	}, [favorites]);
 
-  const retrieveFavorites = useCallback(() => {
-    if (user) {
-      FavoritesDataService.getFavorites(user.googleId)
-        .then(response => {
-            console.log(response);
-            setFavorites(response.data.favorites);
-        })
-        .catch(e => {
-            console.log(e);
-        });
-      }
-  }, [user]);
+	useEffect(() => {
+		retrieveFavorites();
+	}, [retrieveFavorites]);
 
-  const updateFavorites = useCallback(() => {
-    if (user) {
-      FavoritesDataService.updateFavorites({_id: user.googleId, favorites: favorites})
-        .catch(e => { console.log(e); 
-      });
-    }
-  }, [favorites]);
+	useEffect(() => {
+		updateFavorites();
+	}, [updateFavorites]);
 
-  useEffect(() => {
-    retrieveFavorites();
-  }, [retrieveFavorites]);
+	return (
+		<GoogleOAuthProvider clientId={clientId}>
+			<div className="App">
+				<Navbar bg="primary" expand="lg" sticky="top" variant="dark">
+					<Container className="container-fluid">
+						<Navbar.Brand className="brand" href="/">
+							<img
+								src="/images/movies-logo.png"
+								alt="movies logo"
+								className="moviesLogo"
+							/>
+							MOVIE TIME
+						</Navbar.Brand>
+						<Navbar.Toggle aria-controls="basic-navbar-nav" />
+						<Navbar.Collapse id="responsive-navbar-nav">
+							<Nav className="ml-auto">
+								<Nav.Link as={Link} to={"/movies"}>
+									Movies
+								</Nav.Link>
+								{user && (
+									<Nav.Link as={Link} to={"/favorites"}>
+										Favorites
+									</Nav.Link>
+								)}
+							</Nav>
+						</Navbar.Collapse>
+						<Button href="/upload" variant="secondary">
+							Sell
+						</Button>
+						{user ? <Logout setUser={setUser} /> : <Login setUser={setUser} />}
+					</Container>
+				</Navbar>
 
-  useEffect(() => {
-    updateFavorites();
-  }, [updateFavorites]);
-
-  return (
-    <GoogleOAuthProvider clientId={clientId}>
-      <div className="App">
-        <Navbar bg="primary" expand="lg" sticky="top" variant="dark">
-          <Container className="container-fluid">
-            <Navbar.Brand className="brand" href="/">
-              <img src="/images/movies-logo.png" alt="movies logo" className="moviesLogo"/>
-              MOVIE TIME
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav" >
-              <Nav className="ml-auto">
-                <Nav.Link as={Link} to={"/movies"}>
-                  Movies
-                </Nav.Link>
-                {user &&
-                  <Nav.Link as={Link} to={"/favorites"}>
-                    Favorites
-                </Nav.Link>}
-              </Nav>
-            </Navbar.Collapse>
-            { user ? (
-                    <Logout setUser={setUser} />
-                  ) : (
-                    <Login setUser={setUser} />
-                  )}
-          </Container>
-        </Navbar>
-
-        <Routes>
-          <Route exact path={"/"} element={
-            <MoviesList 
-              user={ user }
-              addFavorite={ addFavorite }
-              deleteFavorite={ deleteFavorite }
-              favorites={ favorites }
-            />}
-            />
-          <Route exact path={"/movies"} element={
-            <MoviesList 
-              user={ user }
-              addFavorite={ addFavorite }
-              deleteFavorite={ deleteFavorite }
-              favorites={ favorites }
-            />}
-            />
-          <Route exact path={"/favorites"} element={
-            user ? (
-                  <Favorites user={ user }/>
-                ) : (
-                  <MoviesList 
-                    user={ user }
-                    addFavorite={ addFavorite }
-                    deleteFavorite={ deleteFavorite }
-                    favorites={ favorites }
-                  />
-                )}
-            />
-          <Route path={"/movies/:id/"} element={
-            <Movie user={ user } />}
-            />
-          <Route path={"/movies/:id/review"} element={
-            <AddReview user={ user } />}
-            />
-        </Routes>
-      </div>
-    </GoogleOAuthProvider>
-  );
+				<Routes>
+					<Route
+						exact
+						path={"/"}
+						element={
+							<MoviesList
+								user={user}
+								addFavorite={addFavorite}
+								deleteFavorite={deleteFavorite}
+								favorites={favorites}
+							/>
+						}
+					/>
+					<Route
+						exact
+						path={"/movies"}
+						element={
+							<MoviesList
+								user={user}
+								addFavorite={addFavorite}
+								deleteFavorite={deleteFavorite}
+								favorites={favorites}
+							/>
+						}
+					/>
+					<Route exact path={"/upload"} element={<UploadItem user={user} />} />
+					<Route
+						exact
+						path={"/favorites"}
+						element={
+							user ? (
+								<Favorites user={user} />
+							) : (
+								<MoviesList
+									user={user}
+									addFavorite={addFavorite}
+									deleteFavorite={deleteFavorite}
+									favorites={favorites}
+								/>
+							)
+						}
+					/>
+					<Route path={"/movies/:id/"} element={<Movie user={user} />} />
+					<Route
+						path={"/movies/:id/review"}
+						element={<AddReview user={user} />}
+					/>
+				</Routes>
+			</div>
+		</GoogleOAuthProvider>
+	);
 }
-
 
 export default App;
--- src/components/UploadItem.js
+++ src/components/UploadItem.js
@@ -1,5 +1,74 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import MovieDataService from "../services/movies";
+
 const UploadItem = ({ user }) => {
-	return <h2>Upload items</h2>;
+	const navigate = useNavigate();
+	const [name, setName] = useState("");
+	const [price, setPrice] = useState();
+	const [description, setDescription] = useState("");
+
+	const saveItem = () => {
+		let data = {
+			user_id: user.googleId,
+			name: name,
+			price: price,
+			description: description,
+		};
+
+		MovieDataService.uploadItem(data)
+			.then((res) => navigate("/movies"))
+			.catch((e) => console.log(e));
+	};
+
+	const onChangeName = (e) => {
+		setName(e.target.value);
+	};
+
+	const onChangePrice = (e) => {
+		setPrice(e.target.value);
+	};
+
+	const onChangeDescription = (e) => {
+		setDescription(e.target.value);
+	};
+
+	return (
+		<div>
+			<h2>Create item for sell on this website!</h2>
+			<Container className="main-container">
+				<Form>
+					<Form.Group className="mb-3">
+						<Form.Control
+							type="text"
+							required
+							value={name}
+							onChange={onChangeName}
+						/>
+						<Form.Control
+							type="number"
+							required
+							value={price}
+							onChange={onChangePrice}
+						/>
+						<Form.Control
+							as="textarea"
+							type="text"
+							required
+							value={description}
+							onChange={onChangeDescription}
+						/>
+					</Form.Group>
+					<Button variant="primary" onClick={saveItem}>
+						Submit
+					</Button>
+				</Form>
+			</Container>
+		</div>
+	);
 };
 
 export default UploadItem;
